205d7c302c73e5e459026a8b1f45c0f08e068e53
Sarah H <73975710+Arearda@users.noreply.github.com>
2022-07-07 19:11:37 -0700

    fixing API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const PORT = 8000
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
