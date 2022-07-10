8733ca12bd7f638175d640d9b3fc004b735c5cb4
Sarah H <73975710+Arearda@users.noreply.github.com>
2022-07-10 10:42:03 -0700

    adding collection

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'CampingGearAPI',
-    collection
+    dbName = 'CampingGearAPI'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
-        collection = db.collection('SleepingBags')
+    
 
 //use thie below to create new document in the infoCollection
 // db.collection('infoCollection').insertOne({name: "new", bagbrand: ""})
