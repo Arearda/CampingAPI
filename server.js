1aed95a4d179ac2a76ceff064e3ce3adfe946f94
Sarah H <73975710+Arearda@users.noreply.github.com>
2022-07-10 10:55:21 -0700

    fixing API

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'CampGearAPI'
+    dbName = 'CampGearAPI',
+    collection
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} Database`)
         db = client.db(dbName)
+        collection = db.collection('SleepingBags')
     
 
 //use thie below to create new document in the infoCollection
